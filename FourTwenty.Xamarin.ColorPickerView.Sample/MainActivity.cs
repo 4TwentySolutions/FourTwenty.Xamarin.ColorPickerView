e7495f16be392df61775b90a714cc3a176297666
Anton Yaroshenko <kpakadijl@hotmail.com>
2020-01-01 15:05:46 +0200

    created nuspec

--- FourTwenty.Xamarin.ColorPickerView.Sample/MainActivity.cs
+++ FourTwenty.Xamarin.ColorPickerView.Sample/MainActivity.cs
@@ -2,7 +2,7 @@
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
-using Android.Widget;
+using Xamarin.Essentials;
 
 namespace FourTwenty.Xamarin.ColorPickerView.Sample
 {
@@ -12,13 +12,13 @@ namespace FourTwenty.Xamarin.ColorPickerView.Sample
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            //Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
